a85f90691b2a71f300dad5cea7b19eb1ad884a56
AMWolfstein <ahmedhero4040@gmail.com>
2025-07-12 00:41:57 +0300

    sm7435-common: Add public libraries from stock
    * From paros:15/SUQ32HV-V1-ST14/56b832:user/release-keys
    
    Change-Id: I03815a25c0cc186c0fb3054c8795f775147641cd

--- common.mk
+++ common.mk
@@ -225,6 +225,11 @@ ifeq ($(TARGET_USES_ST_NFC),true)
       android.hardware.nfc-service.st
 endif
 
+# Native libraries
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt \
+    $(LOCAL_PATH)/configs/public.libraries-qti.txt:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/public.libraries-qti.txt
+
 # Overlay
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
