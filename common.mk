eb45a5492b374b47001d7877296d7a16b8c1cac7
Rakhshan Ali <alirakhshan7070@gmail.com>
2025-08-13 15:17:41 +0530

    sm7435-common: Build MotoActions MotoCommonOverlay Enable Moto
    Change-Id: Ia991304678a92ebcae224ba3f00868c19b589520

--- common.mk
+++ common.mk
@@ -213,6 +213,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.keystore.app_attest_key.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.keystore.app_attest_key.xml
 
+# Moto hardware
+PRODUCT_PACKAGES += \
+    MotoActions \
+    MotoCommonOverlay
+
 # Network
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.ipsec_tunnel_migration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnel_migration.xml \
